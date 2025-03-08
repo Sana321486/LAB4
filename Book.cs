beba4996a59f2757fa81236c35086006fa9c7d56
Sana321486 <24ntucsfl1214@student.ntu.edu.pk>
2025-03-08 21:40:45 +0500

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_3
+{
+    internal class Book
+    {
+        public int ISBN;
+        public string title;
+        public int price;
+        public string majorarea;
+        public string subarea;
+        public int numofpg;
+        public void Inputbook()
+        {
+            Console.Write($"Enter ISBN:");
+            ISBN = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Enter Title:");
+            title = Console.ReadLine();
+            Console.Write($"Enter Price:");
+            price = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Enter Major Area:");
+            majorarea = Console.ReadLine();
+            Console.Write($"Enter Sub Area:");
+            subarea = Console.ReadLine();
+            Console.Write($"Enter Number of Pages:");
+            numofpg = Convert.ToInt32(Console.ReadLine());
+        }
+        public void Printbook()
+        {
+            Console.WriteLine($" ISBN:{ISBN}");
+            Console.WriteLine($" Title:{title}");
+            Console.WriteLine($" Price:{price}");
+            Console.WriteLine($" Major Area:{majorarea}");
+            Console.WriteLine($" Sub Area:{subarea}");
+            Console.WriteLine($" Number of Pages:{numofpg}");
+
+        }
+    }
+   
+
+   
+    
+}
+
--- Program.cs
+++ Program.cs
@@ -4,14 +4,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Question_2
+namespace Question_3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Fraction fraction = new Fraction(10, 100);
-            fraction.display();
+            Book[] books = new Book[20];
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("\tEnter Details for 20 Books");
+            Console.WriteLine("--------------------------------------------");
+            for (int i = 1; i <= 20; i++)
+            {
+                Console.WriteLine($"Enter Details for Book {i}");
+                books[i] = new Book();
+                books[i].Inputbook();
+            }
+            Console.WriteLine("All Books");
+            for (int i = 1; i <= 20; i++)
+            {
+                books[i].Printbook();
+            }
+            Console.WriteLine("Enter a book title to search");
+            string searchtitle = Console.ReadLine();
+            searchbook (books, searchtitle);
         }
-    }
+        public static void searchbook(Book[] books, string title)
+        {
+            bool found = false;
+            foreach (Book book in books)
+            {
+                if (book.title == title)
+                {
+                    Console.WriteLine("BOOK FOUND");
+                    book.Printbook();
+                    found = true;
+                    break;
+                }
+                if (!found)
+                {
+                    Console.WriteLine("Book Not Found");
+                }
+            }
+        }
+    } 
 }
+
+
+
+
