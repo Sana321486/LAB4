63fd9d7e8cfd6abfe2031ca1bac0255c89740903
Qadri Laptop <Qadri Laptop@DESKTOP-QRNJE53>
2025-03-08 22:32:05 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Trip trip = new Trip("England", 400, 70, 20);
+            trip.DisplayTripDetails();
+        }
+    }
+}
---
+++ Trip.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Question_1
+{
+    internal class Trip
+    {
+        public string destination;
+        public double distancetravelled;
+        public double totalcostofGasoline;
+        public double numofGallonsconsumed;
+        public Trip(string destination, double distancetravelled, double totalcostofGasoline, double numofGallonsconsumed)
+        {
+            this.destination = destination;
+            this.distancetravelled = distancetravelled;
+            this.totalcostofGasoline = totalcostofGasoline;
+            this.numofGallonsconsumed = numofGallonsconsumed;
+        }
+        public double milespergallon()
+        {
+           return distancetravelled / numofGallonsconsumed;
+        }
+        public double costpermile()
+        {
+            return totalcostofGasoline / distancetravelled;
+        }
+        public void DisplayTripDetails()
+        {
+            Console.WriteLine("*********************************************");
+            Console.WriteLine("\tTrip details are as follow:");
+            Console.WriteLine("*********************************************");
+            Console.WriteLine($"Destination: {destination}");
+            Console.WriteLine($"Distance Travelled: {distancetravelled}");
+            Console.WriteLine($"Total Cost of Gasoline: {totalcostofGasoline}");
+            Console.WriteLine($"Number of Gallons Consumed: {numofGallonsconsumed}");
+            Console.WriteLine($"Miles Per Gallon: {milespergallon()}");
+            Console.WriteLine($"Cost Per Mile: {costpermile()}");
+        }
+    }
+   
+}
