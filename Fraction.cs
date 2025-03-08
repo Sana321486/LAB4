59093cacb232d96cec38dcc53a48ec828c5e66d4
Sana321486 <24ntucsfl1214@student.ntu.edu.pk>
2025-03-08 21:39:56 +0500

    Add files via upload

---
+++ Fraction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_2
+{
+    internal class Fraction
+    {
+        public int dividend;
+        public int divisor;
+        public Fraction(int dividend, int divisor)
+        {
+            this.dividend = dividend;
+            this.divisor = divisor;
+        }
+        public void display()
+        {
+            Console.WriteLine($"{dividend} / {divisor}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Question_1
+namespace Question_2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Trip trip = new Trip("England", 400, 70, 20);
-            trip.DisplayTripDetails();
+            Fraction fraction = new Fraction(10, 100);
+            fraction.display();
         }
     }
 }
