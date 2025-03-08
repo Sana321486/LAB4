6f6bb8d808e7570590fa34b808613a7339139d80
Sana321486 <24ntucsfl1214@student.ntu.edu.pk>
2025-03-08 21:41:27 +0500

    Add files via upload

---
+++ MealCard.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_5
+{
+    internal class MealCard
+    {
+        public string studentname;
+        public int balance;
+        public MealCard(string studentname, int initialbalance = 100)
+        {
+            this.studentname = studentname;
+            this.balance = initialbalance;
+        }
+        public void PurchasePoints(int points)
+        {
+            balance += points;
+            Console.WriteLine($"Point added:{points}");
+            Console.WriteLine($"New Balance:{balance}");
+        }
+            public void buyFood(int cost)
+        {
+            if (balance >= cost)
+            {
+                balance -= cost;
+                Console.WriteLine($"Food purchase for points:{cost}");
+                Console.WriteLine($"Remaining balance:{balance}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid cost amount");
+            }
+        }
+        public void DisplayBalance()
+        {
+            Console.WriteLine($"Current Balance points: {balance}");
+        }
+    }
+}
+      
+
--- Program.cs
+++ Program.cs
@@ -4,52 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Question_3
+namespace Question_5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Book[] books = new Book[20];
-            Console.WriteLine("--------------------------------------------");
-            Console.WriteLine("\tEnter Details for 20 Books");
-            Console.WriteLine("--------------------------------------------");
-            for (int i = 1; i <= 20; i++)
-            {
-                Console.WriteLine($"Enter Details for Book {i}");
-                books[i] = new Book();
-                books[i].Inputbook();
-            }
-            Console.WriteLine("All Books");
-            for (int i = 1; i <= 20; i++)
-            {
-                books[i].Printbook();
-            }
-            Console.WriteLine("Enter a book title to search");
-            string searchtitle = Console.ReadLine();
-            searchbook (books, searchtitle);
+            MealCard card = new MealCard("Sana");
+            card.DisplayBalance();
+            card.buyFood(30);
+            Console.WriteLine("\n");
+            card.DisplayBalance();
+            card.buyFood(80);
+            card.PurchasePoints(50);
+            Console.WriteLine("\n");
+            card.DisplayBalance();
+            card.buyFood(60);
+            card.DisplayBalance();
+
+
         }
-        public static void searchbook(Book[] books, string title)
-        {
-            bool found = false;
-            foreach (Book book in books)
-            {
-                if (book.title == title)
-                {
-                    Console.WriteLine("BOOK FOUND");
-                    book.Printbook();
-                    found = true;
-                    break;
-                }
-                if (!found)
-                {
-                    Console.WriteLine("Book Not Found");
-                }
-            }
-        }
-    } 
+    }
 }
-
-
-
-
